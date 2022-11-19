a36868e98043b3b1da9da9f5f7e054228c5535cf
unknown <okaforchikelue93@gmail.com>
2022-11-19 17:22:36 +0100

    register all models in the two apps in the admin.py file

--- Fleet_Project/Car_Dealer_App/admin.py
+++ Fleet_Project/Car_Dealer_App/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Area, CarDealer, Vehicles
 # Register your models here.
+class AreaAdmin(admin.ModelAdmin):
+    Listdisplay=('pincode', 'city')
+
+class CarDealerAdmin(admin.ModelAdmin):
+    Listdisplay=('car_dealer', 'mobile')
+
+class VehicleAdmin(admin.ModelAdmin):
+    Listdisplay=('car_name', 'color')
+
+admin.site.register(Area, AreaAdmin)
+admin.site.register(CarDealer, CarDealerAdmin)
+admin.site.register(Vehicles, VehicleAdmin)
--- Fleet_Project/Customer_Portal/admin.py
+++ Fleet_Project/Customer_Portal/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer, Orders
+
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    Listdisplay=('user', 'mobile')
+
+class OrdersAdmin(admin.ModelAdmin):
+    Listdisplay=('user', 'vehicle')
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Orders, OrdersAdmin)
